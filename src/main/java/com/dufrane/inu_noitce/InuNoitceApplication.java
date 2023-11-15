50e8028a781e0b8b3355adce1d20dedf1e54b7a2
Dufrane-S <sskim629@gmail.com>
2023-11-15 15:03:45 +0900

    Initial commit

---
+++ src/main/java/com/dufrane/inu_noitce/InuNoitceApplication.java
@@ -0,0 +1,13 @@
+package com.dufrane.inu_noitce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InuNoitceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InuNoitceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dufrane/inu_noitce/InuNoitceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dufrane.inu_noitce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InuNoitceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
